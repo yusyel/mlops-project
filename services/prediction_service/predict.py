f7e58078b654f30d14b7f217b1d0b676f6ff88a9
yusyel <pirekare@outlook.com>
2022-08-06 04:34:10 +0300

    week2

--- services/prediction_service/predict.py
+++ services/prediction_service/predict.py
@@ -15,15 +15,15 @@ best_model = "8c805ce1a2ac4e3198ab41e420396ce7"
 logged_model = f"./1/{best_model}/artifacts/model"
 model = mlflow.sklearn.load_model(logged_model)
 
-MONGODB_ADDRESS = os.getenv('MONGODB_ADDRESS', "mongodb://127.0.0.0.1:27017")
-EVIDENTLY_SERVICE_ADDRESS = os.getenv(
-    'EVIDENTLY_SERVICE_ADDRESS', "http://127.0.0.1:5000")
+EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
+MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27017")
+
 
 
 # %%
 app = Flask('chd')
 mongo_client = MongoClient(MONGODB_ADDRESS)
-db = mongo_client.get_database('chd_risk')
+db = mongo_client.get_database("prediction_service")
 collections = db.get_collection('data')
 
 
@@ -31,15 +31,15 @@ collections = db.get_collection('data')
 def predict():
     record = request.get_json()
 
-    prediction = model.predict_proba(record)[0, 1]
-    risk = prediction >= 0.5
+    y_pred = model.predict_proba(record)[0, 1]
+    risk = y_pred >= 0.5
     result = {
-        'Patients CHD risk probability': float(prediction),
+        'Patients CHD risk probability': float(y_pred),
         'Patient CHD risk result': bool(risk)
     }
+    save_to_db(record, float(y_pred))
+    send_to_evidently_service(record, float(y_pred))
     return jsonify(result)
-    save_to_db(record, prediction)
-    send_to_evidently_service(record, prediction)
 
 
 def save_to_db(record, prediction):
@@ -47,11 +47,10 @@ def save_to_db(record, prediction):
     rec['prediction'] = prediction
     collections.insert_one(rec)
 
-
 def send_to_evidently_service(record, prediction):
     rec = record.copy()
     rec['prediction'] = prediction
-    requests.post(f'{EVIDENTLY_SERVICE_ADDRESS}/itirate/CHD', json=[rec])
+    requests.post(f"{EVIDENTLY_SERVICE_ADDRESS}/iterate/chd", json=[rec])
 
 
 if __name__ == "__main__":
