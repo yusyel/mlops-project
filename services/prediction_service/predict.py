352ccdb8bfb66237d9266944c4af8b99482db4d9
yusyel <pirekare@outlook.com>
2022-08-11 23:24:29 +0300

    func

--- services/prediction_service/predict.py
+++ services/prediction_service/predict.py
@@ -13,17 +13,31 @@ from prefect import flow, task, get_run_logger
 
 
 
-best_model = "285dda4dcdca4cccbac6e8a1f4959e33"
-logged_model = f"./1/{best_model}/artifacts/model"
-model = mlflow.sklearn.load_model(logged_model)
-
-EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
-MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27017")
-
-
 
 # %%
+
+def load_model():
+
+    best_model = "285dda4dcdca4cccbac6e8a1f4959e33"
+    logged_model = f"./1/{best_model}/artifacts/model"
+    model = mlflow.sklearn.load_model(logged_model)
+    return model
+
+
+
+def evidently():
+    EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
+    return EVIDENTLY_SERVICE_ADDRESS
+
+
+
+def mongo():
+    MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27017")
+    return MONGODB_ADDRESS
+
+
 app = Flask('chd')
+MONGODB_ADDRESS = mongo()
 mongo_client = MongoClient(MONGODB_ADDRESS)
 db = mongo_client.get_database("prediction_service")
 collections = db.get_collection('data')
@@ -33,6 +47,7 @@ collections = db.get_collection('data')
 @app.route('/predict', methods=['POST'])
 @flow
 def predict():
+    model = load_model()
     record = request.get_json()
     logger = get_run_logger()
     y_pred = model.predict_proba(record)[0, 1]
@@ -46,19 +61,22 @@ def predict():
     logger.info(f"Result: {result}")
     return jsonify(result)
 
-
-
 @task
 def save_to_db(record, prediction):
+    MONGODB_ADDRESS = mongo()
     rec = record.copy()
     rec['prediction'] = prediction
     collections.insert_one(rec)
 
+
 @task
 def send_to_evidently_service(record, prediction):
+    EVIDENTLY_SERVICE_ADDRESS = evidently()
     rec = record.copy()
     rec['prediction'] = prediction
     requests.post(f"{EVIDENTLY_SERVICE_ADDRESS}/iterate/chd", json=[rec])
+
+
 
 
 if __name__ == "__main__":
